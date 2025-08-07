4cc41d1fba2467b2d971db555830229f60106931
elipauly <elpauly@uiowa.edu>
2025-08-07 12:49:00 -0500

    site mapping

--- website/src/App.js
+++ website/src/App.js
@@ -3,34 +3,39 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  useLocation
 } from "react-router-dom";
 import Home from "./pages";
 import Design from "./pages/design.js";
 import Resume from "./pages/resume.js";
 import Contact from "./pages/contact.js";
 import Uiux from "./pages/uiux.js";
-import Navbar from './components/Navbar.js'; 
+import Navbar from './components/Navbar.js';
 
-
-function App() {
+function AppWrapper() {
   return (
-      <Router>
-          <Navbar />
-          <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/design" element={<Design />} />
-              <Route
-                  path="/uiux"
-                  element={<Uiux />}
-              />
-              <Route path="/resume" element={<Resume />} />
-              <Route
-                  path="/contact"
-                  element={<Contact />}
-              />
-          </Routes>
-      </Router>
+    <Router>
+      <App />
+    </Router>
   );
 }
 
-export default App;
+function App() {
+  const location = useLocation();
+  const excludePaths = ['/'];
+
+  return (
+    <>
+      {!excludePaths.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/design" element={<Design />} />
+        <Route path="/uiux" element={<Uiux />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default AppWrapper;
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,9 +1,29 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
+import { useState, useEffect } from 'react'; 
 
 const Home = () => {
+    const nav = useNavigate();
+    const handleDesign = () =>{
+        nav('/design');
+    };
+    const handleUiux = () =>{
+        nav('/uiux');
+    };
+    const handleResume = () =>{
+        nav('/resume');
+    };
+    const handleContact = () =>{
+        nav('/contact');
+    };
+
     return (
         <div>
-            <h1>pauly</h1>
+            <h1>puly</h1>
+            <button onClick={handleDesign}>to design</button>
+            <button onClick={handleUiux}>to uiux</button>
+            <button onClick={handleResume}>to resume</button>
+            <button onClick={handleContact}>to contact</button>
         </div>
     );
 };
