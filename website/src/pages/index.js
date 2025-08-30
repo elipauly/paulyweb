e1095b8f7fd68441cfa5d6459d6ab9850f817f03
elipauly <elpauly@uiowa.edu>
2025-08-29 23:34:46 -0500

    basic homepage frame

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -18,82 +18,146 @@ const Home = () => {
         nav('/contact');
     };
 
+    //bear with me.
     return (
         <div class="main">
-              <div class="flex-container">
+              <div class="container">
+                <div></div>
+                <div></div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div><div class="square"></div></div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div>4</div>
+                <div>5</div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div><div class="square"></div></div>
+                <div><div class="thirdtop-square"></div></div>
+                <div><div class="square"></div></div>
+                <div>1</div>
+                <div><div class="square"></div></div>
+                <div><div class="thirdtop-square"></div></div>
+                <div><div class="thirdtop-square"></div></div>
+                <div><div class="square"></div></div>
+                <div><div class="thirdtop-square"></div></div>
+                <div><div class="thirdtop-square"></div></div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div>12</div>
+                <div>13</div>
+                <div><div class="thirdtop-square"></div></div>
+                <div>15</div>
+                <div><div class="square"></div></div>
+                <div>1</div>
+                <div><div class="thirdtop-square"></div></div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div><div class="square"></div></div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div><div class="square"></div></div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div><div class="square"></div></div>
+                <div><div class="square"></div></div>
+                <div>15</div>
+                <div><div class="square"></div></div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div><div class="square"></div></div>
+                <div><div class="thirdtop-square"></div></div>
+                <div>14</div>
+                <div><div class="square"></div></div>
+                <div><div class="thirdtop-square"></div></div>
+                <div class="outgrid-text" style={{top:"13vh",left:"33vw",fontSize:"23vh",backgroundColor:"transparent"}}><h2>pauly</h2></div>
+                <div class="outgrid-text" onClick={handleDesign} style={{top:"60vh",left:"8vw",fontSize:"3vh",backgroundColor:"transparent",cursor:"pointer"}}><h2>graphic design</h2></div>
+                <div class="outgrid-text" onClick={handleUiux} style={{top:"37vh",left:"35vw",fontSize:"3vh",backgroundColor:"transparent",cursor:"pointer"}}><h2>ui/ux</h2></div>
+                <div class="outgrid-text" onClick={handleContact} style={{top:"8vh",left:"76vw",fontSize:"3vh",backgroundColor:"transparent",cursor:"pointer"}}><h2>contact</h2></div>
+                <div class="outgrid-text" onClick={handleResume}style={{top:"20vh",left:"60vw",fontSize:"3vh",backgroundColor:"transparent",cursor:"pointer"}}><h2>resume</h2></div>
 
-                <div class="column" style={{paddingTop:"22.1vh"}}>
-                    <div class="square"></div>
-                </div>
-                <div class="column" style={{paddingTop:"22.1vh"}}>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                </div>
-                <div class="column" style={{paddingTop:"22.1vh"}}>
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                     <div class="square"></div>
-                   
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-            
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
-                <div class="column">
-                    <div class="square"></div>
-                    
-                </div>
 
               </div>
-            <h1>puly</h1>
-            <button onClick={handleDesign}>to design</button>
-            <button onClick={handleUiux}>to uiux</button>
-            <button onClick={handleResume}>to resume</button>
-            <button onClick={handleContact}>to contact</button>
         </div>
 
     );
